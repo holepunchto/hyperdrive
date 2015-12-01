3311117e2976a7c15d137bd92662fd38de90e026
Mathias Buus <mathiasbuus@gmail.com>
2015-12-01 16:08:26 +0100

    iterated impl - almost working now

--- README.md
+++ README.md
@@ -9,34 +9,7 @@ npm install hyperdrive
 ## Usage
 
 ``` js
-var hyperdrive = require('hyperdrive')
-var drive = hyperdrive(db, {
-  get: function (name, start, length, cb) {
-    // lookup a requested chunk
-    fs.open(name, 'r', function (err, fd) {
-      if (err) return cb(err)
-      var buf = new Buffer(length)
-      fs.read(fd, buf, 0, length, start, function (err, read) {
-        if (err) return cb(err)
-        length -= read
-        if (length) return fs.read(fd, buf, buf.length - length, length, start += read, read)
-        fs.close(fd, function () {
-          cb(null, buf)
-        })
-      })
-    })
-  }
-})
-
-var files = drive.batch()
-
-fs.createReadStream('test-1.mp4').pipe(batch.add('test-1.mp4', function () {
-  fs.createReadStream('test-2.mp4').pipe(batch.add('test-2.mp4', function () {
-    batch.end(function (err, entry) {
-      console.log('added', entry)
-    })
-  }))
-}))
+// TODO
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,42 +1,76 @@
-var equals = require('buffer-equals')
 var subleveldown = require('subleveldown')
-var hash = require('./lib/hash')
+var pack = require('./lib/pack')
+var feed = require('./lib/feed')
+var swarm = require('./lib/swarm')
 var messages = require('./lib/messages')
-var archive = require('./lib/archive')
 
 module.exports = Hyperdrive
-
-function noop () {}
 
 function Hyperdrive (db, opts) {
   if (!(this instanceof Hyperdrive)) return new Hyperdrive(db, opts)
   if (!opts) opts = {}
 
   this.db = db
-  this._read = opts.read || noop
-  this._write = opts.write || noop
+  this._hashes = subleveldown(db, 'hashes', {valueEncoding: 'binary'})
+  this._blocks = subleveldown(db, 'blocks', {valueEncoding: 'binary'})
+  this._bitfields = subleveldown(db, 'bitfields', {valueEncoding: 'binary'})
+  this._links = subleveldown(db, 'links', {valueEncoding: messages.Link})
 
-  this._trees = subleveldown(db, 'trees', {valueEncoding: messages.TreeInfo})
-  this._byHash = subleveldown(db, 'by-hash', {valueEncoding: messages.TreeNode})
-  this._byIndex = subleveldown(db, 'by-index', {valueEncoding: 'binary'})
-}
-
-Hyperdrive.prototype._readAndVerify = function (name, offset, length, checksum, cb) {
-  this._read(name, offset, length, function (err, buf) {
-    if (err) return cb(err)
-    if (!equals(hash.data(buf), checksum)) return cb(new Error('Checksum mismatch'))
-    cb(null, buf)
-  })
+  this.swarm = swarm(this, opts)
 }
 
 Hyperdrive.prototype.list = function () {
-  return this._trees.createValueStream()
+  return this._links.createKeyStream()
 }
 
-Hyperdrive.prototype.get = function (key) {
-  // stuff ...
+Hyperdrive.prototype.get = function (link) {
+  if (typeof link === 'string') link = new Buffer(link, 'hex')
+  if (typeof link === 'object' && link.id) {
+    return feed(link.id, this, {blocks: link.blocks, index: link.index})
+  }
+  return feed(link, this, {decode: true}) // TODO: be smarter about when to choose to decode? ext maybe?
 }
 
 Hyperdrive.prototype.add = function () {
-  return archive(this)
+  return pack(this)
 }
+
+if (require.main !== module) return
+
+var drive = Hyperdrive(require('level')('test.db'), {name: 'local'})
+var remote = Hyperdrive(require('memdb')(), {name: 'remote'})
+
+var f = remote.get('0fc4a1644ec17df7e69a35a35fe1eb7e3823b41576dc2dab783ea41357a19487')
+
+var s1 = drive.swarm.createStream()
+var s2 = remote.swarm.createStream()
+
+s1.pipe(s2).pipe(s1)
+
+f.get(0, function (err, entry) {
+  console.log(entry)
+  // var nested = drive.get(entry.link)
+})
+
+// drive.list().on('data', function (data) {
+//   console.log(data)
+// })
+
+// var pack = drive.add()
+
+// var s = pack.entry({
+//   name: 'hello.txt',
+//   mode: 100
+// })
+
+// s.write('a')
+// s.write('b')
+// s.write('c')
+// s.write('d')
+// s.write('e')
+
+// s.end()
+
+// pack.finalize(function () {
+//   console.log(pack.blocks, pack.id)
+// })
