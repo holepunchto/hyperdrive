84f36ef71d73a7a3eb293f46aadd25cb9dee8480
Mathias Buus <mathiasbuus@gmail.com>
2015-12-05 03:04:47 +0100

    starting integrating feed-info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var subleveldown = require('subleveldown')
 var pack = require('./lib/pack')
 var feed = require('./lib/feed')
 var swarm = require('./lib/swarm')
+var feedInfo = require('./lib/feed-info')
 var messages = require('./lib/messages')
 
 module.exports = Hyperdrive
@@ -15,6 +16,7 @@ function Hyperdrive (db, opts) {
   this._blocks = subleveldown(db, 'blocks', {valueEncoding: 'binary'})
   this._bitfields = subleveldown(db, 'bitfields', {valueEncoding: 'binary'})
   this._links = subleveldown(db, 'links', {valueEncoding: messages.Link})
+  this._opened = {}
 
   this.swarm = swarm(this, opts)
 }
@@ -37,4 +39,17 @@ Hyperdrive.prototype.get = function (link) {
 
 Hyperdrive.prototype.add = function () {
   return pack(this)
+}
+
+Hyperdrive.prototype._close = function (link) {
+  var id = link.toString('hex')
+  delete this._opened[id]
+}
+
+Hyperdrive.prototype._open = function (link, opts) {
+  var id = link.toString('hex')
+  var info = this._opened[id]
+  if (info) return info
+  this._opened[id] = feedInfo(link, opts)
+  return info
 }
