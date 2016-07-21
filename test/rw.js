3e57825e487e6225c85e29356eb7cfa4e17056ed
James Halliday <mail@substack.net>
2016-07-21 02:30:10 -0700

    failing test for calling createFileReadStream() with previous entries

--- test/rw.js
+++ test/rw.js
@@ -205,3 +205,44 @@ test('write and read after replication', function (t) {
     r.pipe(archive.replicate()).pipe(r)
   })
 })
+
+test.only('read previous entries', function (t) {
+  t.plan(5)
+  var drive = hyperdrive(memdb())
+  var archive = drive.createArchive(null, { live: true })
+  var data = [
+    { 'hello.txt': 'HI' },
+    { 'hello.txt': 'WHAT', 'index.html': '<h1>hey</h1>' },
+    { 'hello.txt': '!' }
+  ]
+  var expected = [
+    { name: 'hello.txt', body: 'HI' },
+    { name: 'hello.txt', body: 'WHAT' },
+    { name: 'index.html', body: '<h1>hey</h1>' },
+    { name: 'hello.txt', body: '!' }
+  ]
+  ;(function next () {
+    if (data.length === 0) return check()
+    var files = data.shift()
+    var pending = 1
+    Object.keys(files).forEach(function (key) {
+      pending++
+      archive.createFileWriteStream(key)
+        .once('finish', done)
+        .end(files[key])
+    })
+    done()
+    function done () { if (--pending === 0) next() }
+  })()
+  function check () {
+    archive.list({ live: false }, function (err, files) {
+      t.error(err)
+      files.forEach(function (file) {
+        archive.createFileReadStream(file).pipe(concat(function (body) {
+          var e = expected.shift()
+          t.equal(body.toString(), e.body, e.name)
+        }))
+      })
+    })
+  }
+})
