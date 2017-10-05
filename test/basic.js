f48fde7a99df04faee43198b30e112d95de3f768
e-e-e <benjamin.mforster@gmail.com>
2017-10-05 22:59:22 +1100

    remove .only from test

--- test/basic.js
+++ test/basic.js
@@ -117,7 +117,7 @@ tape('provide keypair', function (t) {
   })
 })
 
-tape.only('download a version', function (t) {
+tape('download a version', function (t) {
   var src = create()
   src.on('ready', function () {
     t.ok(src.writable)
