df8d845e4f71fbab372b540e21180bccfb05d5c0
Jim Pick <jim@backblend.com>
2018-04-21 01:55:50 -0700

    Pick a version when there are conflicts (#213)
    The existing code doesn't return a value when there is more than one
    choice.

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ Hyperdrive.prototype.ready = function (cb) {
 function reduce (a, b) {
   if (!a.value) return b
   if (!b.value) return a
-  return a.value.mtime < b.value.mtime
+  return a.value.mtime > b.value.mtime ? a : b
 }
 
 function noop () {}
