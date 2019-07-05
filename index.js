f917acb61a104425c410e709c1595465d1040367
RangerMauve <ranger@mauve.moe>
2019-07-05 17:12:56 -0400

    Added tests / fixes

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ function Hyperdrive (storage, key, opts) {
     secretKey: opts.secretKey,
     sparse: opts.sparseMetadata,
     createIfMissing: opts.createIfMissing,
-    storageCacheSize: opts.metadataStorageCacheSize
+    storageCacheSize: opts.metadataStorageCacheSize,
+    extensions: opts.extensions,
   })
   this.content = opts.content || null
   this.maxRequests = opts.maxRequests || 16
