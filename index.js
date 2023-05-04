e33522aaca42e9f598b8dd21a4c9891b21c563f8
Lucas Barrena <lucasbarrena98@gmail.com>
2023-05-04 11:18:52 +0200

    Fix emit blobs and content-key on new drives (#35)

--- index.js
+++ index.js
@@ -145,6 +145,9 @@ module.exports = class Hyperdrive extends ReadyResource {
 
       this.blobs = new Hyperblobs(blobsCore)
       this.db.metadata.contentFeed = this.blobs.core.key
+
+      this.emit('blobs', this.blobs)
+      this.emit('content-key', blobsCore.key)
     }
 
     await this.db.ready()
